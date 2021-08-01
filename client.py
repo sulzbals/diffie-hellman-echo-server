08be1ffce0de0dd9d9b5a051b1bc6fafe2a8666c
Lucas Sulzbach <lucas@sulzbach.org>
2021-07-31 22:35:50 -0300

    Finish Echo Server implementation

--- client.py
+++ client.py
@@ -4,10 +4,10 @@ import socket
 import logging
 import des
 
-FORMAT = '[Echo Client] %(message)s'
+FORMAT = '[CLIENT] [%(levelname)s] %(message)s'
 
 HOST = "127.0.0.1"
-PORT = 50007
+PORT = 50000
 
 MAX_NUM = 1e6
 MIN_NUM = 0
@@ -18,50 +18,98 @@ logger = logging.getLogger("Echo Client")
 
 logger.setLevel(logging.INFO)
 
+a = random.randint(MIN_NUM, MAX_NUM)
+logger.info("Escolhendo a = %d (chave secreta)", a)
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 addr = (HOST, PORT)
 
 sock.connect(addr)
-logger.info("Connected to %s:%d", addr[0], addr[1])
+logger.info("Conexão aberta em %s:%d", addr[0], addr[1])
 
 try:
-    a = random.randint(MIN_NUM, MAX_NUM)
-
     p = sympy.randprime(MIN_NUM, MAX_NUM)
     g = random.randint(MIN_NUM, MAX_NUM) % p
+    logger.info("Escolhendo p = %d e g = %d", p, g)
 
-    msg = p.to_bytes(8, byteorder='big') + g.to_bytes(8, byteorder='big')
-
-    logger.info("p = %d, g = %d", p, g)
+    msg = p.to_bytes(4, byteorder='big') + g.to_bytes(4, byteorder='big')
+    logger.info("Codificando p e g como 0x%s", msg.hex())
 
     sock.sendall(msg)
+    logger.info("Enviando mensagem: 0x%s", msg.hex())
 
     A = g**a % p
+    logger.info("Calculando A = %d**%d %% %d = %d", g, a, p, A)
 
-    msg = A.to_bytes(8, byteorder='big')
-
-    logger.info("A = %d", A)
+    msg = A.to_bytes(4, byteorder='big')
+    logger.info("Codificando A como 0x%s", msg.hex())
 
     sock.sendall(msg)
+    logger.info("Enviando mensagem: 0x%s", msg.hex())
 
-    msg = sock.recv(8)
+    msg = sock.recv(4)
+    logger.info("Mensagem recebida: 0x%s", msg.hex())
 
     B = int.from_bytes(msg, byteorder='big')
-
-    logger.info("B = %d", B)
+    logger.info("Decodificando 0x%s como B = %d", msg.hex(), B)
 
     s = B**a % p
-
-    logger.info("s = %d", s)
+    logger.info("Calculando s = %d**%d %% %d = %d (chave compartilhada)", B, a, p, s)
 
     key = des.DesKey(s.to_bytes(8, byteorder='big'))
 
-    msg = b'Test Message!!!!'
+    logger.info("Testando troca de mensagens encriptadas usando a chave compartilhada s = %d", s)
 
-    logger.info(str(msg, 'utf-8'))
+    test_msg = b'Test Message!!!!'
+    logger.info("Mensagem de teste: %s", str(test_msg, 'utf-8'))
 
-    sock.sendall(key.encrypt(msg))
+    msg = key.encrypt(test_msg)
+    logger.info("Mensagem encriptada: 0x%s", msg.hex())
+
+    sock.sendall(msg)
+    logger.info("Enviando mensagem: 0x%s", msg.hex())
+
+    msg = sock.recv(16)
+    logger.info("Mensagem recebida: 0x%s", msg.hex())
+
+    msg = key.decrypt(msg)
+
+    if msg == test_msg:
+        logger.info("Mensagem decriptada: %s", str(msg, 'utf-8'))
+        logger.info("Mensagem recebida equivale a mensagem enviada")
+    else:
+        logger.info("Mensagem decriptada: 0x%s", msg.hex())
+        logger.error("Mensagem recebida difere da mensagem enviada")
+
+    wrong_s = random.randint(MIN_NUM, MAX_NUM)
+    while wrong_s == s:
+        wrong_s = random.randint(MIN_NUM, MAX_NUM)
+
+    wrong_key = des.DesKey(wrong_s.to_bytes(8, byteorder='big'))
+
+    logger.info("Testando troca de mensagens encriptadas usando s = %d como chave para decriptação (caso de erro)", wrong_s)
+
+    test_msg = b'Test Message!!!!'
+    logger.info("Mensagem de teste: %s", str(test_msg, 'utf-8'))
+
+    msg = key.encrypt(test_msg)
+    logger.info("Mensagem encriptada: 0x%s", msg.hex())
+
+    sock.sendall(msg)
+    logger.info("Enviando mensagem: 0x%s", msg.hex())
+
+    msg = sock.recv(16)
+    logger.info("Mensagem recebida: 0x%s", msg.hex())
+
+    msg = wrong_key.decrypt(msg)
+
+    if msg == test_msg:
+        logger.info("Mensagem decriptada: %s", str(msg, 'utf-8'))
+        logger.info("Mensagem recebida equivale a mensagem enviada")
+    else:
+        logger.info("Mensagem decriptada: 0x%s", msg.hex())
+        logger.error("Mensagem recebida difere da mensagem enviada")
 finally:
-    logger.info("Closing connection")
+    logger.info("Fechando conexão")
     sock.close()
--- server.py
+++ server.py
@@ -3,10 +3,10 @@ import socket
 import logging
 import des
 
-FORMAT = '[Echo Server] %(message)s'
+FORMAT = '[SERVER] [%(levelname)s] %(message)s'
 
 HOST = "127.0.0.1"
-PORT = 50007
+PORT = 50000
 
 MAX_NUM = 1e6
 MIN_NUM = 0
@@ -17,49 +17,71 @@ logger = logging.getLogger("Echo Server")
 
 logger.setLevel(logging.INFO)
 
+b = random.randint(MIN_NUM, MAX_NUM)
+logger.info("Escolhendo b = %d (chave secreta)", b)
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.bind((HOST, PORT))
 sock.listen(1)
-
-logger.info("Listening on %s:%d", HOST, PORT)
+logger.info("Escutando em %s:%d", HOST, PORT)
 
 conn, addr = sock.accept()
-logger.info("Connected to %s:%d", addr[0], addr[1])
+logger.info("Conexão aceita em %s:%d", addr[0], addr[1])
 
 try:
-    b = random.randint(MIN_NUM, MAX_NUM)
-
-    msg = conn.recv(16)
-
-    p = int.from_bytes(msg[:8], byteorder='big')
-    g = int.from_bytes(msg[8:], byteorder='big')
-
-    logger.info("p = %d, g = %d", p, g)
-
     msg = conn.recv(8)
+    logger.info("Mensagem recebida: 0x%s", msg.hex())
+
+    p = int.from_bytes(msg[:4], byteorder='big')
+    g = int.from_bytes(msg[4:], byteorder='big')
+    logger.info("Decodificando 0x%s como p = %d, g = %d", msg.hex(), p, g)
+
+    msg = conn.recv(4)
+    logger.info("Mensagem recebida: 0x%s", msg.hex())
 
     A = int.from_bytes(msg, byteorder='big')
-
-    logger.info("A = %d", A)
+    logger.info("Decodificando 0x%s como A = %d", msg.hex(), A)
 
     B = g**b % p
+    logger.info("Calculando B = %d**%d %% %d = %d", g, b, p, B)
 
-    msg = B.to_bytes(8, byteorder='big')
-
-    logger.info("B = %d", B)
+    msg = B.to_bytes(4, byteorder='big')
+    logger.info("Codificando B como 0x%s", msg.hex())
 
     conn.sendall(msg)
+    logger.info("Enviando mensagem: 0x%s", msg.hex())
 
     s = A**b % p
-
-    logger.info("s = %d", s)
+    logger.info("Calculando s = %d**%d %% %d = %d (chave compartilhada)", A, b, p, s)
 
     key = des.DesKey(s.to_bytes(8, byteorder='big'))
 
-    msg = conn.recv(16)
+    logger.info("Entrando em modo Echo Server")
 
-    logger.info(str(key.decrypt(msg), 'utf-8'))
+    while True:
+        logger.info("Aguardando mensagem")
+
+        msg = conn.recv(16)
+
+        if not msg:
+            break
+
+        logger.info("Mensagem recebida: 0x%s", msg.hex())
+
+        msg = key.decrypt(msg)
+        try:
+            logger.info("Mensagem decriptada: %s", str(msg, 'utf-8'))
+        except UnicodeDecodeError:
+            logger.info("Mensagem decriptada: 0x%s", msg.hex())
+
+        logger.info("Enviando mensagem de volta para o cliente (echo)")
+
+        msg = key.encrypt(msg)
+        logger.info("Mensagem encriptada: 0x%s", msg.hex())
+
+        conn.sendall(msg)
+        logger.info("Enviando mensagem: 0x%s", msg.hex())
 finally:
-    logger.info("Closing connection")
+    logger.info("Fechando conexão")
     conn.close()
